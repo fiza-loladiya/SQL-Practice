e0020a80dd904d391dffdf6410608bc7868f7a05
Fiza <fizaloladiya00@gmail.com>
2026-01-25 17:08:43 +0530

    Fix header formatting in 1st_Excersie.sql

--- 1st_Excersie.sql
+++ 1st_Excersie.sql
@@ -1,4 +1,4 @@
-##Excersie##
+###Excersie###
 select title,release_year from movies where studio = "Marvel Studios";
 select *  from movies where title like "%Avengers%" ;
 select release_year from movies where title like "%The Godfather%";
